57fdaa08a71aa7082a8f20250ad727c0ba35567f
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2022-11-17 22:30:32 +0100

    draft vtp writer

---
+++ gridformat/vtk/vtp_writer.hpp
@@ -0,0 +1,127 @@
+// SPDX-FileCopyrightText: 2022 Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*!
+ * \file
+ * \ingroup VTK
+ * \copydoc GridFormat::VTPWriter
+ */
+#ifndef GRIDFORMAT_VTK_VTP_WRITER_HPP_
+#define GRIDFORMAT_VTK_VTP_WRITER_HPP_
+
+#include <ranges>
+#include <ostream>
+#include <functional>
+
+#include <gridformat/common/ranges.hpp>
+#include <gridformat/common/filtered_range.hpp>
+
+#include <gridformat/grid/grid.hpp>
+#include <gridformat/vtk/common.hpp>
+#include <gridformat/vtk/xml.hpp>
+
+namespace GridFormat {
+
+/*!
+ * \ingroup VTK
+ * \brief Writer for .vtu file format
+ */
+template<Concepts::UnstructuredGrid Grid,
+         typename XMLOpts = VTK::XMLOptions<>,
+         typename PrecOpts = VTK::PrecisionOptions<>>
+class VTPWriter : public VTK::XMLWriterBase<Grid, XMLOpts, PrecOpts> {
+    using ParentType = VTK::XMLWriterBase<Grid, XMLOpts, PrecOpts>;
+
+    template<std::size_t size>
+    struct CellTypesPredicate {
+        std::reference_wrapper<const Grid> grid;
+        std::array<CellType, size> cell_types;
+
+        bool operator()(const Cell<Grid>& cell) const {
+            return std::ranges::any_of(cell_types, [&] (const CellType& _ct) {
+                return _ct == type(grid.get(), cell);
+            });
+        }
+    };
+
+ public:
+    explicit VTPWriter(const Grid& grid,
+                       XMLOpts xml_opts = {},
+                       PrecOpts prec_opts = {})
+    : ParentType(grid, ".vtp", std::move(xml_opts), std::move(prec_opts))
+    {}
+
+ private:
+    using typename ParentType::CoordinateType;
+    using typename ParentType::HeaderType;
+
+    void _write(std::ostream& s) const override {
+        auto verts_range = _get_cell_range(
+            CellTypesPredicate<1>{this->_get_grid(), {CellType::vertex}}
+        );
+        auto lines_range = _get_cell_range(
+            CellTypesPredicate<1>{this->_get_grid(), {CellType::segment}}
+        );
+        auto polys_range = _get_cell_range(
+            CellTypesPredicate<3>{this->_get_grid(), {CellType::quadrilateral, CellType::polygon, CellType::triangle}}
+        );
+
+        const auto num_verts = Ranges::size(verts_range);
+        const auto num_lines = Ranges::size(lines_range);
+        const auto num_polys = Ranges::size(polys_range);
+
+        auto context = this->_get_write_context("PolyData");
+        this->_set_attribute(context, "Piece", "NumberOfPoints", number_of_points(this->_get_grid()));
+        this->_set_attribute(context, "Piece", "NumberOfVerts", num_verts);
+        this->_set_attribute(context, "Piece", "NumberOfLines", num_lines);
+        this->_set_attribute(context, "Piece", "NumberOfStrips", "0");
+        this->_set_attribute(context, "Piece", "NumberOfPolys", num_polys);
+
+        std::ranges::for_each(this->_point_field_names(), [&] (const std::string& n) {
+            this->_set_data_array(context, "Piece.PointData", n, this->_get_point_field(n));
+        });
+        std::ranges::for_each(this->_cell_field_names(), [&] (const std::string& n) {
+            this->_set_data_array(context, "Piece.CellData", n, this->_get_cell_field(n));
+        });
+
+        const auto coords_field = VTK::make_coordinates_field<CoordinateType>(this->_get_grid());
+        this->_set_data_array(context, "Piece.Points", "Coordinates", coords_field);
+
+        if (num_verts > 0) {
+            std::cout << "CALLING VERTS" << std::endl;
+            const auto verts_connectivity_field = VTK::make_connectivity_field<HeaderType>(this->_get_grid(), verts_range);
+            const auto verts_offsets_field = VTK::make_offsets_field<HeaderType>(this->_get_grid(), verts_range);
+            this->_set_data_array(context, "Piece.Verts", "connectivity", verts_connectivity_field);
+            this->_set_data_array(context, "Piece.Verts", "offsets", verts_offsets_field);
+            std::cout << "DONE" << std::endl;
+        }
+
+        if (num_lines > 0) {
+            std::cout << "CALLING LINES" << std::endl;
+            const auto lines_connectivity_field = VTK::make_connectivity_field<HeaderType>(this->_get_grid(), lines_range);
+            const auto lines_offsets_field = VTK::make_offsets_field<HeaderType>(this->_get_grid(), lines_range);
+            this->_set_data_array(context, "Piece.Lines", "connectivity", lines_connectivity_field);
+            this->_set_data_array(context, "Piece.Lines", "offsets", lines_offsets_field);
+            std::cout << "DONE" << std::endl;
+        }
+
+        if (num_polys > 0) {
+            std::cout << "CALLING POLYS" << std::endl;
+            const auto polys_connectivity_field = VTK::make_connectivity_field<HeaderType>(this->_get_grid(), polys_range);
+            const auto polys_offsets_field = VTK::make_offsets_field<HeaderType>(this->_get_grid(), polys_range);
+            this->_set_data_array(context, "Piece.Polys", "connectivity", polys_connectivity_field);
+            this->_set_data_array(context, "Piece.Polys", "offsets", polys_offsets_field);
+            std::cout << "DONE" << std::endl;
+        }
+
+        this->_write_xml(std::move(context), s);
+    }
+
+    template<typename Predicate>
+    std::ranges::forward_range auto _get_cell_range(Predicate&& pred) const {
+        return filtered(cells(this->_get_grid()), std::forward<Predicate>(pred));
+    }
+};
+
+}  // namespace GridFormat
+
+#endif  // GRIDFORMAT_VTK_VTP_WRITER_HPP_
---
+++ test/vtk/test_vtp_writer.cpp
@@ -0,0 +1,75 @@
+#include <gridformat/vtk/vtp_writer.hpp>
+
+#include "../grid/unstructured_grid.hpp"
+#include "../make_test_data.hpp"
+
+template<typename XMLOptions,
+         typename PrecisionOptions,
+         typename FieldPrecision = GridFormat::Precision<double>>
+void write([[maybe_unused]] const XMLOptions& xml_opts,
+           [[maybe_unused]] const PrecisionOptions& prec_opts,
+           [[maybe_unused]] const std::string& filename,
+           [[maybe_unused]] const FieldPrecision& prec = {}) {
+    const auto grid = GridFormat::Test::make_unstructured_2d();
+    auto point_scalars = GridFormat::Test::make_point_data<double>(grid);
+    auto point_vectors = GridFormat::Test::make_vector_data(point_scalars);
+    auto point_tensors = GridFormat::Test::make_tensor_data(point_scalars);
+    auto cell_scalars = GridFormat::Test::make_cell_data<double>(grid);
+    auto cell_vectors = GridFormat::Test::make_vector_data(cell_scalars);
+    auto cell_tensors = GridFormat::Test::make_tensor_data(cell_scalars);
+
+    GridFormat::VTPWriter writer{grid, xml_opts, prec_opts};
+    writer.set_point_field("pscalar", [&] (const auto& p) { return point_scalars[p.id]; });
+    writer.set_point_field("pvector", [&] (const auto& p) { return point_vectors[p.id]; });
+    writer.set_point_field("ptensor", [&] (const auto& p) { return point_tensors[p.id]; });
+    writer.set_cell_field("cscalar", [&] (const auto& c) { return cell_scalars[c.id]; });
+    writer.set_cell_field("cvector", [&] (const auto& c) { return cell_vectors[c.id]; });
+    writer.set_cell_field("ctensor", [&] (const auto& c) { return cell_tensors[c.id]; });
+
+    writer.set_point_field("pscalar_custom_prec", [&] (const auto& p) { return point_scalars[p.id]; }, prec);
+    writer.set_point_field("pvector_custom_prec", [&] (const auto& p) { return point_vectors[p.id]; }, prec);
+    writer.set_point_field("ptensor_custom_prec", [&] (const auto& p) { return point_tensors[p.id]; }, prec);
+    writer.set_cell_field("cscalar_custom_prec", [&] (const auto& c) { return cell_scalars[c.id]; }, prec);
+    writer.set_cell_field("cvector_custom_prec", [&] (const auto& c) { return cell_vectors[c.id]; }, prec);
+    writer.set_cell_field("ctensor_custom_prec", [&] (const auto& c) { return cell_tensors[c.id]; }, prec);
+
+    std::cout << "Wrote " << writer.write(filename) << ".vtu" << std::endl;
+}
+
+int main() {
+    write(GridFormat::VTK::XMLOptions{}, GridFormat::VTK::PrecisionOptions{}, "vtp_default");
+    write(
+        GridFormat::VTK::XMLOptions{.encoder = GridFormat::Encoding::ascii},
+        GridFormat::VTK::PrecisionOptions{},
+        "vtp_ascii"
+    );
+    write(
+        GridFormat::VTK::XMLOptions{
+            .encoder = GridFormat::Encoding::base64,
+            .data_format = GridFormat::VTK::DataFormat::inlined
+        },
+        GridFormat::VTK::PrecisionOptions{},
+        "vtp_base64_inlined"
+    );
+    write(
+        GridFormat::VTK::XMLOptions{
+            .encoder = GridFormat::Encoding::base64,
+            .compression = GridFormat::Compression::lzma,
+            .data_format = GridFormat::VTK::DataFormat::inlined
+        },
+        GridFormat::VTK::PrecisionOptions{},
+        "vtp_base64_inlined_lzma_compression"
+    );
+    write(
+        GridFormat::VTK::XMLOptions{
+            .encoder = GridFormat::Encoding::base64,
+            .compression = GridFormat::Compression::lzma,
+            .data_format = GridFormat::VTK::DataFormat::inlined
+        },
+        GridFormat::VTK::PrecisionOptions{},
+        "vtp_base64_inlined_lzma_compression_custom_field_precision",
+        GridFormat::Precision<float>{}
+    );
+
+    return 0;
+}
