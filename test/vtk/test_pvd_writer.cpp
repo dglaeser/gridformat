0aaeed62f9bfcda7f9b7ffc9c7c7db9b64a31ce6
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2022-11-24 20:22:59 +0100

    [test][pvd] print written filenames

--- test/vtk/test_pvd_writer.cpp
+++ test/vtk/test_pvd_writer.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 
 #include <gridformat/grid.hpp>
+#include <gridformat/common/logging.hpp>
 #include <gridformat/vtk/pvd_writer.hpp>
 #include <gridformat/vtk/vtu_writer.hpp>
 #include "../grid/unstructured_grid.hpp"
@@ -16,8 +17,8 @@ void test(const Grid& grid, Writer& pvd_writer) {
 
     pvd_writer.set_point_field("pdata", [&] (const auto& p) { return point_data[p.id]; });
     pvd_writer.set_cell_field("cdata", [&] (const auto& c) { return cell_data[c.id]; });
-    pvd_writer.write(0.0);
-
+    std::string filename = pvd_writer.write(0.0);
+    std::cout << "Wrote '" << GridFormat::as_highlight(filename) << "'" << std::endl;
     double sim_time = 0.0;
     const double end_time = 10.0;
     const double timestep_size = 1.0;
@@ -25,20 +26,21 @@ void test(const Grid& grid, Writer& pvd_writer) {
         std::ranges::for_each(point_data, [] (double& v) { v*= 2.0; });
         std::ranges::for_each(cell_data, [] (double& v) { v*= 2.0; });
         sim_time += timestep_size;
-        pvd_writer.write(sim_time);
+        filename = pvd_writer.write(sim_time);
+        std::cout << "Wrote '" << GridFormat::as_highlight(filename) << "'" << std::endl;
     }
 }
 
 template<typename Grid>
 void test_from_instance(const Grid& grid) {
-    GridFormat::PVDWriter pvd_writer{GridFormat::VTUWriter{grid}, "pvd_time_series"};
+    GridFormat::PVDWriter pvd_writer{GridFormat::VTUWriter{grid}, "pvd_time_series_2d_in_2d"};
     test(grid, pvd_writer);
 }
 
 template<typename Grid>
 void test_from_abstract_base_ptr(const Grid& grid) {
     using BaseWriter = GridFormat::TimeSeriesGridWriter<Grid>;
-    auto writer = GridFormat::PVDWriter{GridFormat::VTUWriter{grid}, "pvd_time_series_from_base_writer"};
+    auto writer = GridFormat::PVDWriter{GridFormat::VTUWriter{grid}, "pvd_time_series_from_base_writer_2d_in_2d"};
     std::unique_ptr<BaseWriter> pvd_writer = std::make_unique<decltype(writer)>(std::move(writer));
     test(grid, *pvd_writer);
 }
