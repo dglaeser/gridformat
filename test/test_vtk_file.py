3623ef92d9c7b3aae2ddad257c0880df43dd4527
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2022-11-25 08:41:15 +0100

    [test][vtk] add regression support for pvd

--- test/test_vtk_file.py
+++ test/test_vtk_file.py
@@ -1,10 +1,12 @@
 from os.path import splitext
 from argparse import ArgumentParser
-from typing import Callable, Tuple
+from dataclasses import dataclass
+from typing import Callable, Tuple, List
 from math import sin, cos, isclose
+from xml.etree import ElementTree
 from sys import exit
 
-class TestFunction:
+class _TestFunction:
     def __init__(self, scaling: float = 1.0) -> None:
         self._scaling = scaling
 
@@ -14,78 +16,13 @@ class TestFunction:
             result *= cos(point[1])
         if len(point) > 2:
             result *= point[2] + 1.0
-        return result
+        return result*self._scaling
 
 
-def get_grid_and_space(filename: str) -> Tuple[int, int]:
-    dim, space_dim = filename.split("_in_")
-    dim = dim[-2:-1]
-    space_dim = space_dim[0]
-    return int(dim), int(space_dim)
-
-
-def check_vtk_file(filename: str,
-                   reference_function: Callable[[list], float]) -> None:
-    rel_tol = 1e-5
-    abs_tol = 1e-3
-    _, space_dim = get_grid_and_space(filename)
-
-    try:
-        from vtk import vtkXMLUnstructuredGridReader, vtkXMLPolyDataReader, vtkIdList
-    except ImportError:
-        print("VTK not found")
-        exit(255)
-
-    ext = splitext(filename)[1]
-    if ext == ".vtu":
-        reader = vtkXMLUnstructuredGridReader()
-    elif ext == ".vtp":
-        reader = vtkXMLPolyDataReader()
-    else:
-        print(f"Extension {ext} not yet supported")
-        exit(255)
-    reader.SetFileName(filename)
-    reader.Update()
-    output = reader.GetOutput()
-    points = output.GetPoints()
-
-    def _compute_cell_center(cell_id: int, ) -> tuple:
-        ids = vtkIdList()
-        output.GetCellPoints(cell_id, ids)
-        result = (0., 0., 0.)
-        for i in range(ids.GetNumberOfIds()):
-            result = _add_points(result, points.GetPoint(ids.GetId(i)))
-        return _scale_point(result, 1.0/float(ids.GetNumberOfIds()))
-
-    point_data = output.GetPointData()
-    for i in range(point_data.GetNumberOfArrays()):
-        name = point_data.GetArrayName(i)
-        arr = point_data.GetArray(i)
-        print(f"Comparing point array '{name}'")
-        for i in range(arr.GetNumberOfTuples()):
-            point = points.GetPoint(i)
-            value = arr.GetTuple(i)
-            reference = reference_function(_restrict_to_space_dim(point, space_dim))
-            for comp in range(arr.GetNumberOfComponents()):
-                if comp/3 < space_dim and comp%3 < space_dim:
-                    assert isclose(reference, value[comp], rel_tol=rel_tol, abs_tol=abs_tol)
-                else:
-                    assert isclose(0.0, value[comp], rel_tol=rel_tol, abs_tol=abs_tol)
-
-    cell_data = output.GetCellData()
-    for i in range(cell_data.GetNumberOfArrays()):
-        name = cell_data.GetArrayName(i)
-        arr = cell_data.GetArray(i)
-        print(f"Comparing cell array '{name}'")
-        for i in range(arr.GetNumberOfTuples()):
-            point = _compute_cell_center(i)
-            value = arr.GetTuple(i)
-            reference = reference_function(_restrict_to_space_dim(point, space_dim))
-            for comp in range(arr.GetNumberOfComponents()):
-                if comp/3 < space_dim and comp%3 < space_dim:
-                    assert isclose(reference, value[comp], rel_tol=rel_tol, abs_tol=abs_tol)
-                else:
-                    assert isclose(0.0, value[comp], rel_tol=rel_tol, abs_tol=abs_tol)
+@dataclass
+class _TimeStep:
+    filename: str
+    time: float
 
 
 def _add_points(p0: tuple, p1: tuple) -> tuple:
@@ -106,8 +43,103 @@ def _restrict_to_space_dim(point: tuple, space_dim: int) -> tuple:
         return tuple([point[0], point[1], point[2]])
 
 
+def _get_grid_and_space_dimension(filename: str) -> Tuple[int, int]:
+    dim, space_dim = filename.split("_in_")
+    dim = dim[-2:-1]
+    space_dim = space_dim[0]
+    return int(dim), int(space_dim)
+
+
+def _check_vtk_file(vtk_reader,
+                    space_dim,
+                    reference_function: Callable[[list], float]) -> None:
+    rel_tol = 1e-5
+    abs_tol = 1e-3
+
+    vtk_reader.Update()
+    output = vtk_reader.GetOutput()
+    points = output.GetPoints()
+
+    def _compute_cell_center(cell_id: int, ) -> tuple:
+        from vtk import vtkIdList
+        ids = vtkIdList()
+        output.GetCellPoints(cell_id, ids)
+        result = (0., 0., 0.)
+        for i in range(ids.GetNumberOfIds()):
+            result = _add_points(result, points.GetPoint(ids.GetId(i)))
+        return _scale_point(result, 1.0/float(ids.GetNumberOfIds()))
+
+    def _compare_data_array(arr, position_call_back):
+        for i in range(arr.GetNumberOfTuples()):
+            point = position_call_back(i)
+            value = arr.GetTuple(i)
+            reference = reference_function(_restrict_to_space_dim(point, space_dim))
+            for comp in range(arr.GetNumberOfComponents()):
+                if comp/3 < space_dim and comp%3 < space_dim:
+                    assert isclose(reference, value[comp], rel_tol=rel_tol, abs_tol=abs_tol)
+                else:
+                    assert isclose(0.0, value[comp], rel_tol=rel_tol, abs_tol=abs_tol)
+
+    point_data = output.GetPointData()
+    for i in range(point_data.GetNumberOfArrays()):
+        name = point_data.GetArrayName(i)
+        arr = point_data.GetArray(i)
+        print(f"Comparing point array '{name}'")
+        for i in range(arr.GetNumberOfTuples()):
+            _compare_data_array(arr, lambda i: points.GetPoint(i))
+
+    cell_data = output.GetCellData()
+    for i in range(cell_data.GetNumberOfArrays()):
+        name = cell_data.GetArrayName(i)
+        arr = cell_data.GetArray(i)
+        print(f"Comparing cell array '{name}'")
+        for i in range(arr.GetNumberOfTuples()):
+            _compare_data_array(arr, lambda i: _compute_cell_center(i))
+
+
+def _read_pvd_pieces(filename: str) -> List[_TimeStep]:
+    tree = ElementTree.parse(filename)
+    root = tree.getroot()
+    return [
+        _TimeStep(
+            filename=data_set.attrib["file"],
+            time=data_set.attrib["timestep"]
+        ) for data_set in root.find("Collection")
+    ]
+
+
+def _test_vtk(filename: str, reference_function: Callable[[list], float]):
+    try:
+        from vtk import vtkXMLUnstructuredGridReader, vtkXMLPolyDataReader
+    except ImportError:
+        print("VTK not found")
+        exit(255)
+
+    ext = splitext(filename)[1]
+    if ext == ".vtu":
+        reader = vtkXMLUnstructuredGridReader()
+    elif ext == ".vtp":
+        reader = vtkXMLPolyDataReader()
+    reader.SetFileName(filename)
+    _, space_dim = _get_grid_and_space_dimension(filename)
+    _check_vtk_file(reader, space_dim, reference_function)
+
+
+def test(filename: str) -> None:
+    ext = splitext(filename)[1]
+    if ext in [".vtu", ".vtp"]:
+        _test_vtk(filename, _TestFunction())
+    elif ext == ".pvd":
+        for timestep in _read_pvd_pieces(filename):
+            print(f"Comparing timestep '{timestep.time}' in file {timestep.filename}")
+            _test_vtk(timestep.filename, _TestFunction(float(timestep.time)))
+    else:
+        print(f"No reader for files with extension {ext}")
+        exit(255)
+
+
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("-p", "--filepath", required=True)
     args = vars(parser.parse_args())
-    check_vtk_file(args["filepath"], TestFunction())
+    test(args["filepath"])
