3982d85a84c263ef5bc0ed0c9ede79a00ff28322
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2023-04-02 16:54:11 +0200

    [ranges] add convenience func for sort+unique

--- gridformat/common/ranges.hpp
+++ gridformat/common/ranges.hpp
@@ -9,7 +9,9 @@
 #define GRIDFORMAT_COMMON_RANGES_HPP_
 
 #include <ranges>
+#include <utility>
 #include <iterator>
+#include <functional>
 #include <type_traits>
 
 #include <gridformat/common/concepts.hpp>
@@ -46,6 +48,18 @@ constexpr auto size(R&& r) {
 template<Concepts::StaticallySizedRange R>
 constexpr auto size(R&&) {
     return StaticSize<std::decay_t<R>>::value;
+}
+
+/*!
+ * \ingroup Common
+ * \brief Sort the given range and remove all duplicates.
+ */
+template<std::ranges::range R,
+         typename Comp = std::ranges::less,
+         typename EqPredicate = std::equal_to<std::ranges::range_value_t<R>>>
+constexpr decltype(auto) sort_and_unique(R&& r, Comp comparator = {}, EqPredicate eq = {}) {
+    std::ranges::sort(std::forward<R>(r), comparator);
+    return std::ranges::unique(std::forward<R>(r), eq);
 }
 
 }  // namespace GridFormat::Ranges
