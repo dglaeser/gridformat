56b64bd7a1250085eaea9b8bbd8f452d257e3868
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2023-05-07 08:54:00 +0200

    [test][vts] test rectangular & smaller grids

--- test/vtk/test_vts_writer.cpp
+++ test/vtk/test_vts_writer.cpp
@@ -9,22 +9,51 @@
 #include "../grid/structured_grid.hpp"
 #include "vtk_writer_tester.hpp"
 
-template<int dim>
-void _test(const std::array<std::size_t, dim>& cells, bool invert = false) {
-    std::array<double, dim> size;
-    std::ranges::fill(size, 1.0);
-    auto grid = GridFormat::Test::StructuredGrid<dim>{size, cells};
-    if (invert)
-        grid.invert();
-    GridFormat::Test::VTK::WriterTester tester{std::move(grid), ".vts", true, (invert ? "inverted" : "")};
+template<typename Grid>
+void _test(Grid&& grid, const std::string& suffix = "") {
+    GridFormat::Test::VTK::WriterTester tester{std::move(grid), ".vts", true, suffix};
     tester.test([&] (const auto& grid, const auto& xml_opts) {
         return GridFormat::VTSWriter{grid, xml_opts};
     });
 }
 
 int main() {
-    _test<2>({10, 10});
-    _test<3>({10, 10, 10});
-    _test<3>({10, 10, 10}, true);
+    for (std::size_t nx : {2})
+        for (std::size_t ny : {2, 3}) {
+            const auto base_suffix = std::to_string(nx) + "_" + std::to_string(ny);
+            _test(
+                GridFormat::Test::StructuredGrid<2>{{{1.0, 1.0}}, {{nx, ny}}},
+                base_suffix
+            );
+
+            _test(
+                GridFormat::Test::StructuredGrid<2>{{{1.0, 1.0}}, {{nx, ny}}, {{1.0, 1.0}}},
+                base_suffix + "_shifted"
+            );
+
+            GridFormat::Test::StructuredGrid<2> inverted{{{1.0, 1.0}}, {{nx, ny}}, {{1.0, 1.0}}};
+            inverted.invert();
+            _test(std::move(inverted), base_suffix + "_inverted");
+        }
+
+    for (std::size_t nx : {2})
+        for (std::size_t ny : {2, 3})
+            for (std::size_t nz : {2, 4}) {
+                const auto base_suffix = std::to_string(nx) + "_" + std::to_string(ny) + "_" + std::to_string(nz);
+                _test(
+                    GridFormat::Test::StructuredGrid<3>{{{1.0, 1.0, 1.0}}, {{nx, ny, nz}}},
+                    base_suffix
+                );
+
+                _test(
+                    GridFormat::Test::StructuredGrid<3>{{{1.0, 1.0, 1.0}}, {{nx, ny, nz}}, {{1.0, 1.0, 1.0}}},
+                    base_suffix + "_shifted"
+                );
+
+                GridFormat::Test::StructuredGrid<3> inverted{{{1.0, 1.0, 1.0}}, {{nx, ny, nz}}, {{1.0, 1.0, 1.0}}};
+                inverted.invert();
+                _test(std::move(inverted), base_suffix + "_inverted");
+            }
+
     return 0;
 }
