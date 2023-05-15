871608cc757ce5fc717d49065d960fa2e9c62ea3
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2023-05-15 10:49:05 +0200

    [api] restructure generic writer

--- gridformat/writer.hpp
+++ gridformat/writer.hpp
@@ -8,56 +8,119 @@
 #ifndef GRIDFORMAT_WRITER_HPP_
 #define GRIDFORMAT_WRITER_HPP_
 
-#include <cmath>
 #include <memory>
-#include <concepts>
-#include <type_traits>
-#include <ranges>
 #include <utility>
 
-#include <gridformat/common/exceptions.hpp>
-#include <gridformat/common/logging.hpp>
-
-#include <gridformat/grid/grid.hpp>
 #include <gridformat/grid/writer.hpp>
-#include <gridformat/grid/cell_type.hpp>
-
 #include <gridformat/grid/concepts.hpp>
+#include <gridformat/grid/type_traits.hpp>
 #include <gridformat/parallel/concepts.hpp>
-
-#include <gridformat/vtk/vtu_writer.hpp>
-#include <gridformat/vtk/vtp_writer.hpp>
-#include <gridformat/vtk/pvtu_writer.hpp>
-#include <gridformat/vtk/pvd_writer.hpp>
 
 namespace GridFormat {
 
-enum class FileFormat {
-    vtu,
-    vtp
-};
+template<typename FileFormat>
+struct WriterFactory;
+
+
+#ifndef DOXYGEN
+namespace Detail {
+
+    template<typename FileFormat, typename Grid>
+    static constexpr bool has_sequential_factory
+        = is_complete<WriterFactory<FileFormat>>
+        and requires(const FileFormat& f, const Grid& grid) {
+            { WriterFactory<FileFormat>::make(f, grid) } -> std::derived_from<GridWriter<Grid>>;
+        };
+
+    template<typename FileFormat, typename Grid, typename Comm>
+    static constexpr bool has_parallel_factory
+        = is_complete<WriterFactory<FileFormat>>
+        and requires(const FileFormat& f, const Grid& grid, const Comm& comm) {
+            { WriterFactory<FileFormat>::make(f, grid, comm) } -> std::derived_from<GridWriter<Grid>>;
+        };
+
+}  // namespace Detail
+#endif  // DOXYGEN
 
 
 template<Concepts::Grid Grid>
 class Writer {
  public:
+    template<typename FileFormat>
+        requires(Detail::has_sequential_factory<FileFormat, Grid>)
+    Writer(const FileFormat& f, const Grid& grid)
+    : Writer(WriterFactory<FileFormat>::make(f, grid))
+    {}
+
+    template<typename FileFormat, Concepts::Communicator Comm>
+        requires(Detail::has_parallel_factory<FileFormat, Grid, Comm>)
+    Writer(const FileFormat& f, const Grid& grid, const Comm& comm)
+    : Writer(WriterFactory<FileFormat>::make(f, grid, comm))
+    {}
+
     template<std::derived_from<GridWriter<Grid>> W> requires(!std::is_lvalue_reference_v<W>)
     explicit Writer(W&& writer)
     : _writer(std::make_unique<W>(std::move(writer)))
     {}
 
-    template<typename... Args>
-    void set_point_field(Args&&... args) {
-        _writer->set_point_field(std::forward<Args>(args)...);
-    }
-
-    template<typename... Args>
-    void set_cell_field(Args&&... args) {
-        _writer->set_cell_field(std::forward<Args>(args)...);
-    }
-
-    std::string write(const std::string& filename) {
+    std::string write(const std::string& filename) const {
         return _writer->write(filename);
+    }
+
+    template<typename Field>
+    void set_meta_data(const std::string& name, Field&& field) {
+        _writer->set_meta_data(name, std::forward<Field>(field));
+    }
+
+    template<typename Field>
+    void set_point_field(const std::string& name, Field&& field) {
+        _writer->set_point_field(name, std::forward<Field>(field));
+    }
+
+    template<typename Field>
+    void set_cell_field(const std::string& name, Field&& field) {
+        _writer->set_cell_field(name, std::forward<Field>(field));
+    }
+
+    FieldPtr remove_meta_data(const std::string& name) {
+        return _writer->remove_meta_data(name);
+    }
+
+    FieldPtr remove_point_field(const std::string& name) {
+        return _writer->remove_point_field(name);
+    }
+
+    FieldPtr remove_cell_field(const std::string& name) {
+        return _writer->remove_cell_field(name);
+    }
+
+    void clear() {
+        _writer->clear();
+    }
+
+    template<typename Writer>
+    void copy_fields(Writer& w) const {
+        _writer->copy_fields(w);
+    }
+
+    const std::optional<WriterOptions>& writer_options() const {
+        return _writer->writer_options();
+    }
+
+    const Grid& grid() const {
+        return _writer->grid();
+    }
+
+    friend decltype(auto) point_fields(const Writer& w) {
+        return point_fields(*w._writer);
+    }
+
+    friend decltype(auto) cell_fields(const Writer& w) {
+        return cell_fields(*w._writer);
+    }
+
+    friend decltype(auto) meta_data_fields(const Writer& w) {
+        return meta_data_fields(*w._writer);
     }
 
  private:
@@ -65,122 +128,36 @@ class Writer {
 };
 
 
-template<Concepts::Grid Grid>
-class TimeSeriesWriter {
- public:
-    template<std::derived_from<TimeSeriesGridWriter<Grid>> W> requires(!std::is_lvalue_reference_v<W>)
-    explicit TimeSeriesWriter(W&& writer)
-    : _writer(std::make_unique<W>(std::move(writer)))
-    {}
+// template<Concepts::Grid Grid>
+// class TimeSeriesWriter {
+//  public:
+//     template<std::derived_from<TimeSeriesGridWriter<Grid>> W> requires(!std::is_lvalue_reference_v<W>)
+//     explicit TimeSeriesWriter(W&& writer)
+//     : _writer(std::make_unique<W>(std::move(writer)))
+//     {}
 
-    template<typename... Args>
-    void set_point_field(Args&&... args) {
-        _writer->set_point_field(std::forward<Args>(args)...);
-    }
+//     template<typename... Args>
+//     void set_meta_data(Args&&... args) {
+//         _writer->set_meta_data(std::forward<Args>(args)...);
+//     }
 
-    template<typename... Args>
-    void set_cell_field(Args&&... args) {
-        _writer->set_cell_field(std::forward<Args>(args)...);
-    }
+//     template<typename... Args>
+//     void set_point_field(Args&&... args) {
+//         _writer->set_point_field(std::forward<Args>(args)...);
+//     }
 
-    std::string write(double t) {
-        return _writer->write(t);
-    }
+//     template<typename... Args>
+//     void set_cell_field(Args&&... args) {
+//         _writer->set_cell_field(std::forward<Args>(args)...);
+//     }
 
- private:
-    std::unique_ptr<TimeSeriesGridWriter<Grid>> _writer;
-};
+//     std::string write(double t) {
+//         return _writer->write(t);
+//     }
 
-
-#ifndef DOXYGEN
-namespace Detail {
-
-template<Concepts::UnstructuredGrid Grid>
-inline int max_cell_dim(const Grid& grid) {
-    int max_dim = 0;
-    std::ranges::for_each(cells(grid), [&] (const auto& cell) {
-        max_dim = std::max(cell_dimension(type(grid, cell)), max_dim);
-    });
-    return max_dim;
-}
-
-}  // namespace Detail
-#endif  // DOXYGEN
-
-
-//! Return a default writer for an unstructured grid
-template<Concepts::UnstructuredGrid Grid>
-Writer<Grid> make_writer(const Grid& grid) {
-    if (Detail::max_cell_dim(grid) > 2)
-        return Writer<Grid>{VTUWriter{grid}};
-    return Writer<Grid>{VTPWriter{grid}};
-}
-
-
-//! Return a writer for the given unstructured grid
-template<Concepts::UnstructuredGrid Grid>
-Writer<Grid> make_writer(const Grid& grid, FileFormat format) {
-    if (format == FileFormat::vtp) {
-        if (Detail::max_cell_dim(grid) > 2)
-            as_warning("vtp file format only supports up to 2d cells. 3d cells will be omitted");
-        return Writer<Grid>{VTPWriter{grid}};
-    }
-    if (format == FileFormat::vtu)
-        return Writer<Grid>{VTUWriter{grid}};
-
-    throw InvalidState(as_error("Factory function not yet available for given file format"));
-}
-
-
-//! Return a default parallel writer for the given unstructured grid & communicator
-template<Concepts::UnstructuredGrid Grid, Concepts::Communicator Comm>
-Writer<Grid> make_writer(const Grid& grid, const Comm& comm) {
-    return Writer<Grid>{PVTUWriter{grid, comm}};
-}
-
-
-//! Return a parallel writer for the given unstructured grid & communicator
-template<Concepts::UnstructuredGrid Grid, Concepts::Communicator Comm>
-Writer<Grid> make_writer(const Grid& grid, const Comm& comm, FileFormat format) {
-    if (format == FileFormat::vtu)
-        return Writer<Grid>{PVTUWriter{grid, comm}};
-    throw InvalidState(as_error("Factory function not yet available for given file format"));
-}
-
-
-//! Return a default time series writer for unstructured grids
-template<Concepts::UnstructuredGrid Grid>
-TimeSeriesWriter<Grid> make_time_series_writer(const std::string& base_filename, const Grid& grid) {
-    if (Detail::max_cell_dim(grid) > 2)
-        return TimeSeriesWriter<Grid>{PVDWriter{VTUWriter{grid}, base_filename}};
-    return TimeSeriesWriter<Grid>{PVDWriter{VTPWriter{grid}, base_filename}};
-}
-
-
-//! Return a time series writer
-template<Concepts::Grid Grid>
-TimeSeriesWriter<Grid> make_time_series_writer(const std::string& base_filename,
-                                               const Grid& grid,
-                                               FileFormat format) {
-    throw NotImplemented(as_error("TimeSeriesWriter Factory function with format specification not yet available"));
-}
-
-
-//! Return a default parallel time series writer for unstructured grids
-template<Concepts::UnstructuredGrid Grid, Concepts::Communicator Comm>
-TimeSeriesWriter<Grid> make_time_series_writer(const std::string& base_filename, const Grid& grid, const Comm& comm) {
-    return TimeSeriesWriter<Grid>{PVDWriter{PVTUWriter{grid, comm}, base_filename}};
-}
-
-
-//! Return a parallel time series writer
-template<Concepts::Grid Grid, Concepts::Communicator Comm>
-TimeSeriesWriter<Grid> make_time_series_writer(const std::string& base_filename,
-                                               const Grid& grid,
-                                               const Comm& comm,
-                                               FileFormat format) {
-    throw NotImplemented(as_error("TimeSeriesWriter Factory function with format specification not yet available"));
-}
+//  private:
+//     std::unique_ptr<TimeSeriesGridWriter<Grid>> _writer;
+// };
 
 }  // namespace GridFormat
 
