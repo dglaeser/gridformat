f2fc1c6623ae841704ec6dc95ccc1c123ff573c5
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2023-05-15 12:47:09 +0200

    [test][api] test generic time series writer

--- test/api/test_generic_time_series_writer.cpp
+++ test/api/test_generic_time_series_writer.cpp
@@ -1,26 +1,35 @@
 // SPDX-FileCopyrightText: 2022 Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-#include <gridformat/writer.hpp>
-#include "../grid/unstructured_grid.hpp"
+#include <gridformat/gridformat.hpp>
+
 #include "../make_test_data.hpp"
+
+template<typename Writer>
+void write(Writer&& writer) {
+    const auto& grid = writer.grid();
+    GridFormat::Test::add_meta_data(writer);
+    for (double sim_time : {0.0, 0.5, 1.0}) {
+        writer.set_point_field("point_func", [&] (const auto& p) {
+            return GridFormat::Test::test_function<double>(grid.position(p))*sim_time;
+        });
+        writer.set_cell_field("cell_func", [&] (const auto& c) {
+            return GridFormat::Test::test_function<double>(grid.center(c))*sim_time;
+        });
+        std::cout << "Writing at t = " << sim_time << std::endl;
+        std::cout << "Wrote '" << GridFormat::as_highlight(writer.write(sim_time)) << "'" << std::endl;
+    }
+}
 
 int main() {
     using GridFormat::Test::test_function;
-    using GridFormat::Test::_compute_cell_center;
 
-    double sim_time = 0.0;
-    auto grid = GridFormat::Test::make_unstructured_2d<2>();
-    auto series_writer = GridFormat::make_time_series_writer("generic_time_series_2d_in_2d", grid);
-    series_writer.set_point_field("point_func", [&] (const auto& p) {
-        return test_function<double>(GridFormat::coordinates(grid, p))*sim_time;
-    });
-    series_writer.set_cell_field("cell_func", [&] (const auto& c) {
-        return test_function<double>(_compute_cell_center(grid, c))*sim_time;
-    });
-    series_writer.write(0.0);
-    sim_time = 1.0;
-    series_writer.write(1.0);
+    GridFormat::ImageGrid<2, double> grid{{1.0, 1.0}, {4, 5}};
+    write(GridFormat::Writer{GridFormat::pvd(GridFormat::vtu), grid, "generic_time_series_2d_in_2d_vtu"});
+    write(GridFormat::Writer{GridFormat::pvd(GridFormat::vti), grid, "generic_time_series_2d_in_2d_vti"});
+    write(GridFormat::Writer{GridFormat::pvd(GridFormat::vtr), grid, "generic_time_series_2d_in_2d_vtr"});
+    write(GridFormat::Writer{GridFormat::pvd(GridFormat::vtk_hdf), grid, "generic_time_series_2d_in_2d_unstructured_vtk_hdf"});
+    write(GridFormat::Writer{GridFormat::pvd(GridFormat::vts({.encoder = GridFormat::Encoding::ascii})), grid, "generic_time_series_2d_in_2d_vts"});
 
     return 0;
 }
