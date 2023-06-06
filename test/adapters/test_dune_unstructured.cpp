f42a28feac1ab119b141347e1d5222667eb9b8f4
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2023-06-06 13:31:04 +0200

    [test] test dune with discontinuous output

--- test/adapters/test_dune_unstructured.cpp
+++ test/adapters/test_dune_unstructured.cpp
@@ -8,8 +8,11 @@
 #pragma GCC diagnostic pop
 
 #include <gridformat/grid/adapters/dune.hpp>
+#include <gridformat/grid/discontinuous.hpp>
+
 #include <gridformat/vtk/vtu_writer.hpp>
 #include <gridformat/vtk/vtp_writer.hpp>
+
 #include "../vtk/vtk_writer_tester.hpp"
 #include "../make_test_data.hpp"
 
@@ -40,6 +43,19 @@ int main(int argc, char** argv) {
         return GridFormat::Test::test_function<double>(element.geometry().center());
     });
     poly_writer.write("dune_vtu_2d_in_2d_as_poly");
+
+    // test discontinuous output to check that wrapper works with dune grids
+    GridFormat::DiscontinuousGrid discontinuous_grid{grid_view};
+    GridFormat::VTUWriter discontinuous_writer{discontinuous_grid};
+    GridFormat::Test::add_meta_data(discontinuous_writer);
+    GridFormat::Test::add_discontinuous_point_field(discontinuous_writer);
+    discontinuous_writer.set_point_field("accessor_test", [&] (const auto& p) {
+        [[maybe_unused]] const auto& c = p.cell();
+        [[maybe_unused]] const auto& hc = p.host_cell();
+        [[maybe_unused]] const auto& i = p.index_in_host();
+        return p.cell().index();
+    });
+    discontinuous_writer.write("dune_vtu_2d_in_2d_discontinuous");
 
     return 0;
 }
