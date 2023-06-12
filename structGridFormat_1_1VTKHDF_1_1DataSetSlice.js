cd7b55491c759362d774875a92a1faff9410b1f6
dglaeser <dennis.glaeser@iws.uni-stuttgart.de>
2023-06-12 12:32:16 +0000

    Deploying to gh-pages from @ dglaeser/gridformat@726d1891120175a839e59e1e8938d957fb461f45 🚀

--- structGridFormat_1_1VTKHDF_1_1DataSetSlice.js
+++ structGridFormat_1_1VTKHDF_1_1DataSetSlice.js
@@ -2,5 +2,5 @@ var structGridFormat_1_1VTKHDF_1_1DataSetSlice =
 [
     [ "count", "structGridFormat_1_1VTKHDF_1_1DataSetSlice.html#a57cf97fa11d6184d5706063b8c34e161", null ],
     [ "offset", "structGridFormat_1_1VTKHDF_1_1DataSetSlice.html#ad9e334343d2d8ae8a6af1acc391ee6f9", null ],
-    [ "size", "structGridFormat_1_1VTKHDF_1_1DataSetSlice.html#adeaa4e1ee0244afb98b14ab54f5af148", null ]
+    [ "total_size", "structGridFormat_1_1VTKHDF_1_1DataSetSlice.html#a8c472e34f5d7eae19dc7ff3f1d20c757", null ]
 ];
