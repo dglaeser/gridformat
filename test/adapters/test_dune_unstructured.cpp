15588d292ad80296040ce2451173efa6da95c4e6
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2023-06-18 21:26:09 +0200

    [test][dune] test 3d unstructured grid output

--- test/adapters/test_dune_unstructured.cpp
+++ test/adapters/test_dune_unstructured.cpp
@@ -19,14 +19,13 @@
 #include "../vtk/vtk_writer_tester.hpp"
 #include "../make_test_data.hpp"
 
-int main(int argc, char** argv) {
-    Dune::MPIHelper::instance(argc, argv);
-    Dune::YaspGrid<2> grid{
-        {1.0, 1.0},
-        {10, 10}
-    };
+template<typename GridView>
+void test(const GridView& grid_view) {
+    std::string base_filename =
+        "dune_vtu_"
+        + std::to_string(GridView::dimension) + "d_in_"
+        + std::to_string(GridView::dimensionworld) + "d";
 
-    const auto& grid_view = grid.leafGridView();
     GridFormat::VTUWriter writer{grid_view};
     GridFormat::Test::add_meta_data(writer);
     writer.set_point_field("pfunc", [&] (const auto& vertex) {
@@ -35,17 +34,19 @@ int main(int argc, char** argv) {
     writer.set_cell_field("cfunc", [&] (const auto& element) {
         return GridFormat::Test::test_function<double>(element.geometry().center());
     });
-    writer.write("dune_vtu_2d_in_2d");
+    std::cout << "Wrote '" << writer.write(base_filename) << "'" << std::endl;
 
-    GridFormat::VTPWriter poly_writer{grid_view};
-    GridFormat::Test::add_meta_data(poly_writer);
-    poly_writer.set_point_field("pfunc", [&] (const auto& vertex) {
-        return GridFormat::Test::test_function<double>(vertex.geometry().center());
-    });
-    poly_writer.set_cell_field("cfunc", [&] (const auto& element) {
-        return GridFormat::Test::test_function<double>(element.geometry().center());
-    });
-    poly_writer.write("dune_vtu_2d_in_2d_as_poly");
+    if (GridView::dimension < 3) {
+        GridFormat::VTPWriter poly_writer{grid_view};
+        GridFormat::Test::add_meta_data(poly_writer);
+        poly_writer.set_point_field("pfunc", [&] (const auto& vertex) {
+            return GridFormat::Test::test_function<double>(vertex.geometry().center());
+        });
+        poly_writer.set_cell_field("cfunc", [&] (const auto& element) {
+            return GridFormat::Test::test_function<double>(element.geometry().center());
+        });
+        std::cout << "Wrote '" << poly_writer.write(base_filename + "_as_poly") << "'" << std::endl;
+    }
 
     // test discontinuous output to check that wrapper works with dune grids
     GridFormat::DiscontinuousGrid discontinuous_grid{grid_view};
@@ -58,7 +59,14 @@ int main(int argc, char** argv) {
         [[maybe_unused]] const auto& i = p.index_in_host();
         return p.cell().index();
     });
-    discontinuous_writer.write("dune_vtu_2d_in_2d_discontinuous");
+    std::cout << "Wrote '" << discontinuous_writer.write(base_filename + "_discontinuous") << "'" << std::endl;
+}
 
+int main(int argc, char** argv) {
+    Dune::MPIHelper::instance(argc, argv);
+    Dune::YaspGrid<2> grid_2d{{1.0, 1.0}, {6, 7}};
+    Dune::YaspGrid<3> grid_3d{{1.0, 1.0, 1.0}, {5, 6, 7}};
+    test(grid_2d.leafGridView());
+    test(grid_3d.leafGridView());
     return 0;
 }
