36d5b20eee5a067ab910832984e57958bcf6335b
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2023-06-21 09:47:35 +0200

    [examples] add higher-order output to dune example

--- examples/example4/example4.cpp
+++ examples/example4/example4.cpp
@@ -5,13 +5,15 @@
 #include <ranges>
 
 #include <dune/grid/yaspgrid.hh>
+#include <dune/functions/gridfunctions/analyticgridviewfunction.hh>
 
 #include <gridformat/traits/dune.hpp>
 #include <gridformat/grid/discontinuous.hpp>
 #include <gridformat/gridformat.hpp>
 
-int main() {
-    // create a grid with 100x100*100 cells on the unit cube
+
+// Exemplary usage of writing grid views...
+void write_grid_view() {
     Dune::YaspGrid<3> grid{{1.0, 1.0, 1.0}, {100, 100, 100}};
     const auto& grid_view = grid.leafGridView();
 
@@ -33,7 +35,6 @@ int main() {
         return center[0]*center[1];
     });
 
-
     // Typically, you would use Dune for numerical simulations in which you store a discrete solution
     // in some vector. We can simply access those values in the lambda we set as point/cell fields.
     // In this example, we just fill those vectors with indices...
@@ -51,31 +52,31 @@ int main() {
 
     const auto filename = writer.write("dune_yasp");
     std::cout << "Wrote '" << filename << "'" << std::endl;
+}
 
 
-    // GridFormat also provides a wrapper around unstructured grids in order to produce discontinuous
-    // output. That is, output in which there are different values for points depending on the cell in
-    // which they are embedded in. What this wrapper effectively does is to join the cell and cell point
-    // iterators, thereby visiting each point from all cells connected to it. Note that if your grid structure
-    // is already represented like this, you don't need to wrap it in order to get discontinuous output...
-
-    // first of all, let's make a small 2d grid so the result will be easier to inspect...
-    Dune::YaspGrid<2> small_grid{{1.0, 1.0}, {10, 10}};
-    const auto& small_grid_view = small_grid.leafGridView();
+// Example to show how discontinuous output can be achieved
+// GridFormat provides a wrapper around unstructured grids in order to produce discontinuous
+// output. That is, output in which there are different values for points depending on the cell in
+// which they are embedded in. What this wrapper effectively does is to join the cell and cell point
+// iterators, thereby visiting each point from all cells connected to it.
+void write_discontinuous_grid_view() {
+    Dune::YaspGrid<2> grid{{1.0, 1.0}, {10, 10}};
+    const auto& grid_view = grid.leafGridView();
 
     // Let's say we store a discontinuous solution in a vector of vectors. In this example, we
     // simply store the element index on each point of a cell s.t. we see a discontinuous field
     // when opening the resulting file...
-    std::vector<std::vector<double>> discontinuous_solution(small_grid_view.size(0));
-    for (const auto& element : elements(small_grid_view)) {
+    std::vector<std::vector<double>> discontinuous_solution(grid_view.size(0));
+    for (const auto& element : elements(grid_view)) {
         const auto num_corners = element.subEntities(2);
-        const auto element_index = small_grid_view.indexSet().index(element);
+        const auto element_index = grid_view.indexSet().index(element);
         discontinuous_solution[element_index].resize(num_corners);
         std::ranges::fill(discontinuous_solution[element_index], element_index);
     }
 
     // We can now simply wrap our grid view as a discontinuous grid and write it
-    const auto discontinuous = GridFormat::make_discontinuous(small_grid_view);
+    const auto discontinuous = GridFormat::make_discontinuous(grid_view);
     GridFormat::Writer discontinuous_writer{GridFormat::vtu, discontinuous};
     discontinuous_writer.set_point_field("cell_index_at_points", [&] (const auto& p) {
         // what we receive here is a point that provides access to the cell it is embedded in
@@ -83,13 +84,46 @@ int main() {
         [[maybe_unused]] const auto& host_point = p.host_point();
         const auto& element = p.host_cell();
         const auto local_index = p.index_in_host();
-        const auto element_index = small_grid_view.indexSet().index(element);
+        const auto element_index = grid_view.indexSet().index(element);
         return discontinuous_solution[element_index][local_index];
     });
 
     // The written file should show piecewise-constant indices per cell - but as point data
     const auto discontinuous_filename = discontinuous_writer.write("dune_yasp_discontinuous");
     std::cout << "Wrote '" << discontinuous_filename << "'" << std::endl;
+}
 
+
+void write_higher_order_dune_function() {
+    Dune::YaspGrid<3> grid{{1.0, 1.0, 1.0}, {20, 20, 20}};
+    const auto& grid_view = grid.leafGridView();
+
+    // To use higher-order output, let us wrap the grid view in the provided LagrangeMesh,
+    // which exposes a mesh consisting of lagrange cells with the specified order
+    unsigned int order = 2;
+    GridFormat::Dune::LagrangeMesh lagrange_mesh{grid_view, 2};
+    GridFormat::Writer writer{GridFormat::default_for(lagrange_mesh), lagrange_mesh};
+
+    // we can now use convenience functions to add Dune::Functions, evaluated at the
+    // points/cells of the lagrange mesh, to the writer...
+    // First, let's create a Dune::Function:
+    auto function = Dune::Functions::makeAnalyticGridViewFunction([] (const auto& position) {
+        return position[0]*position[1];
+    }, grid_view);
+
+    // then, let's add this both as point and cell field
+    GridFormat::Dune::set_point_function(function, writer, "point_function");
+    GridFormat::Dune::set_cell_function(function, writer, "cell_function");
+
+    // ... and write the file
+    const auto higher_order_filename = writer.write("dune_quadratic_function");
+    std::cout << "Wrote '" << higher_order_filename << "'" << std::endl;
+}
+
+
+int main() {
+    write_grid_view();
+    write_discontinuous_grid_view();
+    write_higher_order_dune_function();
     return 0;
 }
