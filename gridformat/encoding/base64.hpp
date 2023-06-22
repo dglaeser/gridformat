e33eb23ae4e509e8719210ed54202a264fe80fb0
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2023-06-22 17:21:58 +0200

    [encoding][base64] add cache option

--- gridformat/encoding/base64.hpp
+++ gridformat/encoding/base64.hpp
@@ -8,34 +8,30 @@
 #ifndef GRIDFORMAT_COMMON_ENCODING_BASE64_HPP_
 #define GRIDFORMAT_COMMON_ENCODING_BASE64_HPP_
 
+#include <vector>
+#include <utility>
 #include <cassert>
 #include <algorithm>
 
 #include <gridformat/common/exceptions.hpp>
 #include <gridformat/common/output_stream.hpp>
 
-#ifndef GRIDFORMAT_BASE64_NUM_CACHED_TRIPLETS
-#define GRIDFORMAT_BASE64_NUM_CACHED_TRIPLETS 4000  // yields ~16MB cache
-#endif
-
 namespace GridFormat {
 
 //! \addtogroup Encoding
 //! \{
 
+struct Base64EncoderOptions {
+    std::size_t num_cached_buffers = 4000;  //!< Number of triplets cached between write operations
+};
+
 //! Wrapper around a given stream to write output encoded with base64
 template<typename OStream>
 class Base64Stream : public OutputStreamWrapperBase<OStream> {
     using Byte = char;
-
+    static_assert(sizeof(std::byte) == sizeof(Byte));
     static constexpr int buffer_size = 3;
     static constexpr int encoded_buffer_size = 4;
-    static constexpr int num_cached_buffers = GRIDFORMAT_BASE64_NUM_CACHED_TRIPLETS;
-    static constexpr int cache_size_in = num_cached_buffers*buffer_size;
-    static constexpr int cache_size_out = num_cached_buffers*encoded_buffer_size;
-
-    static_assert(num_cached_buffers > 0);
-    static_assert(sizeof(std::byte) == sizeof(Byte));
 
     static constexpr unsigned char base64_alphabet[] = {
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
@@ -65,8 +61,9 @@ class Base64Stream : public OutputStreamWrapperBase<OStream> {
     }
 
  public:
-    explicit Base64Stream(OStream& s)
+    explicit Base64Stream(OStream& s, Base64EncoderOptions opts = {})
     : OutputStreamWrapperBase<OStream>(s)
+    , _opts{std::move(opts)}
     {}
 
     template<typename T, std::size_t size>
@@ -77,20 +74,23 @@ class Base64Stream : public OutputStreamWrapperBase<OStream> {
     }
 
  private:
+    std::size_t _cache_size_in() const { return _opts.num_cached_buffers*buffer_size; }
+    std::size_t _cache_size_out() const { return _opts.num_cached_buffers*encoded_buffer_size; }
+
     void _write(const Byte* data, std::size_t size) {
         const auto num_full_buffers = size/buffer_size;
-        const auto num_full_caches = num_full_buffers/num_cached_buffers;
+        const auto num_full_caches = num_full_buffers/_opts.num_cached_buffers;
         for (const auto i : std::views::iota(std::size_t{0}, num_full_caches))
-            _flush_full_cache(data + i*cache_size_in);
+            _flush_full_cache(data + i*_cache_size_in());
 
-        const auto processed_bytes = num_full_caches*cache_size_in;
+        const auto processed_bytes = num_full_caches*_cache_size_in();
         if (size > processed_bytes)
             _flush_cache(data + processed_bytes, size - processed_bytes);
     }
 
     void _flush_full_cache(const Byte* data) {
-        Byte cache[cache_size_out];
-        for (std::size_t i = 0; i < cache_size_out/encoded_buffer_size; ++i) {
+        std::vector<Byte> cache(_cache_size_out());
+        for (std::size_t i = 0; i < _cache_size_out()/encoded_buffer_size; ++i) {
             const std::size_t in_offset = i*buffer_size;
             const std::size_t out_offset = i*encoded_buffer_size;
             cache[out_offset + 0] = _encodeSextet0(data + in_offset);
@@ -104,13 +104,13 @@ class Base64Stream : public OutputStreamWrapperBase<OStream> {
     void _flush_cache(const Byte* data, std::size_t num_bytes_in) {
         if (num_bytes_in == 0)
             return;
-        if (num_bytes_in > cache_size_in)
+        if (num_bytes_in > _cache_size_in())
             throw SizeError("Number of bytes cannot be larger than cache size");
 
         const std::size_t num_full_buffers = num_bytes_in/buffer_size;
         const std::size_t residual = num_bytes_in%buffer_size;
 
-        Byte cache[cache_size_out];
+        std::vector<Byte> cache(_cache_size_out());
         for (std::size_t i = 0; i < num_full_buffers; ++i) {
             const std::size_t in_offset = i*buffer_size;
             const std::size_t out_offset = i*encoded_buffer_size;
@@ -132,11 +132,13 @@ class Base64Stream : public OutputStreamWrapperBase<OStream> {
             cache[out_offset + 1] = _encodeSextet1(last_buffer);
             cache[out_offset + 2] = residual > 1 ? _encodeSextet2(last_buffer) : '=';
             cache[out_offset + 3] = residual > 2 ? _encodeSextet3(last_buffer) : '=';
-            this->_stream.write(std::span{cache, out_offset + 4});
+            this->_stream.write(std::span{cache.data(), out_offset + 4});
         } else {
-            this->_stream.write(std::span{cache, out_offset});
+            this->_stream.write(std::span{cache.data(), out_offset});
         }
     }
+
+    Base64EncoderOptions _opts;
 };
 
 namespace Encoding {
@@ -146,8 +148,25 @@ struct Base64 {
     //! Return a base64 stream
     template<typename Stream>
     constexpr auto operator()(Stream& s) const noexcept {
-        return Base64Stream{s};
+        return Base64Stream{s, _opts};
     }
+
+    //! Return an encoder instance with different options
+    constexpr auto operator()(Base64EncoderOptions opts) const {
+        Base64 other;
+        other._opts = std::move(opts);
+        return other;
+    }
+
+    //! Return a base64 encoder with the given options
+    static Base64 with(Base64EncoderOptions opts) {
+        Base64 enc;
+        enc._opts = std::move(opts);
+        return enc;
+    }
+
+ private:
+    Base64EncoderOptions _opts = {};
 };
 
 inline constexpr Base64 base64;  //!< Instance of the base64 encoder
