a28b4b0b5614a893d77323ab0c85f582392450a4
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2023-06-27 09:18:37 +0200

    [concepts] add resizable range

--- gridformat/common/concepts.hpp
+++ gridformat/common/concepts.hpp
@@ -53,6 +53,7 @@ concept MDRange = std::ranges::range<T> and mdrange_dimension<T> == dim;
 
 #ifndef DOXYGEN
 namespace Detail {
+
     template<typename T>
     struct HasStaticallySizedSubRanges : public std::false_type {};
     template<std::ranges::range R> requires(!has_sub_range<R> and has_static_size<R>)
@@ -62,14 +63,22 @@ namespace Detail {
         static constexpr bool value = HasStaticallySizedSubRanges<std::ranges::range_value_t<R>>::value;
     };
 
+    inline constexpr std::size_t undefined_dim = std::numeric_limits<std::size_t>::max();
+
 }  // namespace Detail
 #endif  // DOXYGEN
 
-template<typename T, std::size_t dim = std::numeric_limits<std::size_t>::max()>
+template<typename T, std::size_t dim = Detail::undefined_dim>
 concept StaticallySizedMDRange
     = StaticallySizedRange<T>
     and Detail::HasStaticallySizedSubRanges<T>::value
-    and (MDRange<T, dim> or dim == std::numeric_limits<std::size_t>::max());
+    and (MDRange<T, dim> or dim == Detail::undefined_dim);
+
+template<typename T, std::size_t dim = Detail::undefined_dim>
+concept ResizableMDRange
+    = std::ranges::range<T>
+    and requires(T& t) { { t.resize(std::size_t{}) }; }
+    and (MDRange<T, dim> or dim == Detail::undefined_dim);
 
 template<typename T>
 concept Scalar = is_scalar<T>;
