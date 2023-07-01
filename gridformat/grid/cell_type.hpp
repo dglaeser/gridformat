f52442db3fad956a2740d7ace4dee487ffd618f9
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2023-07-01 08:20:41 +0200

    [cell_type] make streamable

--- gridformat/grid/cell_type.hpp
+++ gridformat/grid/cell_type.hpp
@@ -8,6 +8,7 @@
 #ifndef GRIDFORMAT_GRID_CELL_TYPE_HPP_
 #define GRIDFORMAT_GRID_CELL_TYPE_HPP_
 
+#include <ostream>
 #include <gridformat/common/exceptions.hpp>
 
 namespace GridFormat {
@@ -35,6 +36,31 @@ enum class CellType {
     lagrange_tetrahedron,
     lagrange_hexahedron
 };
+
+std::string as_string(const CellType& ct) {
+    switch (ct) {
+        case CellType::vertex: return "vertex";
+        case CellType::segment: return "segment";
+        case CellType::triangle: return "triangle";
+        case CellType::pixel: return "pixel";
+        case CellType::quadrilateral: return "quadrilateral";
+        case CellType::polygon: return "polygon";
+        case CellType::tetrahedron: return "tetrahedron";
+        case CellType::hexahedron: return "hexahedron";
+        case CellType::voxel: return "voxel";
+        case CellType::lagrange_segment: return "lagrange_segment";
+        case CellType::lagrange_triangle: return "lagrange_triangle";
+        case CellType::lagrange_quadrilateral: return "lagrange_quadrilateral";
+        case CellType::lagrange_tetrahedron: return "lagrange_tetrahedron";
+        case CellType::lagrange_hexahedron: return "lagrange_hexahedron";
+        default: throw NotImplemented("String representation for given cell type");
+    }
+}
+
+std::ostream& operator<<(std::ostream& s, const CellType& ct) {
+    s << as_string(ct);
+    return s;
+}
 
 }  // namespace GridFormat
 
