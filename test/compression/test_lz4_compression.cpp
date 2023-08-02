ae8558fb40b89090d2d83f10624d18a6a3abf118
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2023-08-02 17:16:16 +0200

    [compression] test decompression

--- test/compression/test_lz4_compression.cpp
+++ test/compression/test_lz4_compression.cpp
@@ -1,6 +1,9 @@
 // SPDX-FileCopyrightText: 2022-2023 Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
 // SPDX-License-Identifier: MIT
 
+#include <vector>
+#include <algorithm>
+
 #include <gridformat/common/serialization.hpp>
 #include <gridformat/compression/lz4.hpp>
 #include "../testing.hpp"
@@ -8,6 +11,7 @@
 int main() {
     using GridFormat::Testing::operator""_test;
     using GridFormat::Testing::expect;
+    using GridFormat::Testing::eq;
 
     "lz4_compression_default_opts"_test = [] () {
         GridFormat::Serialization bytes{1000};
@@ -31,6 +35,40 @@ int main() {
         expect(block_sizes.compressed_size() <= 1000);
         expect(block_sizes.number_of_blocks == 4);
         expect(block_sizes.residual_block_size == 100);
+    };
+
+    "lz4_decompression_default"_test = [] () {
+        const std::vector<int> data{42, 43, 44, 45, 56, 66};
+        const auto number_of_bytes = data.size()*sizeof(int);
+
+        GridFormat::Serialization bytes{number_of_bytes};
+        std::ranges::for_each(
+            bytes.template as_span_of<int>(),
+            [&, i=int{0}] (int& value) mutable { value = data[i++]; }
+        );
+
+        GridFormat::Compression::LZ4 compressor;
+        const auto blocks = compressor.compress(bytes);
+        compressor.decompress(bytes, blocks);
+        expect(eq(bytes.size(), number_of_bytes));
+        expect(std::ranges::equal(bytes.template as_span_of<int>(), data));
+    };
+
+    "lz4_decompression_multiple_blocks"_test = [] () {
+        const std::vector<int> data{42, 43, 44, 45, 56, 66};
+        const auto number_of_bytes = data.size()*sizeof(int);
+
+        GridFormat::Serialization bytes{number_of_bytes};
+        std::ranges::for_each(
+            bytes.template as_span_of<int>(),
+            [&, i=int{0}] (int& value) mutable { value = data[i++]; }
+        );
+
+        GridFormat::Compression::LZ4 compressor{{.block_size = number_of_bytes/3}};
+        const auto blocks = compressor.compress(bytes);
+        compressor.decompress(bytes, blocks);
+        expect(eq(bytes.size(), number_of_bytes));
+        expect(std::ranges::equal(bytes.template as_span_of<int>(), data));
     };
 
     return 0;
