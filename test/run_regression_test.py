48368df5d9888191394b491dfcb734a663c6c22f
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2023-08-07 15:27:42 +0200

    [test][regression] ensure error codes propagate

--- test/run_regression_test.py
+++ test/run_regression_test.py
@@ -9,12 +9,12 @@ from fnmatch import fnmatch
 from sys import exit
 
 
-def run_and_catch_error_code(cmd: list) -> int:
+def run_and_catch_error_code(cmd: list) -> tuple[Exception | None, int]:
     try:
         run(cmd, check=True)
     except CalledProcessError as e:
-        return e.returncode
-    return 0
+        return e, e.returncode
+    return None, 0
 
 
 if __name__ == "__main__":
@@ -30,19 +30,28 @@ if __name__ == "__main__":
     if not exists(vtk_check_script):
         raise RuntimeError("Could not find vtk test script")
 
-    ret_code = run_and_catch_error_code(args["command"].split(" "))
+    e, ret_code = run_and_catch_error_code(args["command"].split(" "))
+    if ret_code not in [0, 42]:
+        print(f"Error upon test execution: {e}")
+        exit(ret_code)
+
+    ret_codes = [ret_code]
     env_variable_control_name = "GRIDFORMAT_REGRESSION_SAMPLES_ONLY"
     take_single_sample = True if getenv(env_variable_control_name, "false").lower() in ["true", "1"] else False
     for _file in filter(lambda _f: isfile(_f), listdir(".")):
         if fnmatch(_file, f"{args['regex']}"):
             print(f"Regression testing '{_file}'")
-            ret_code = max(ret_code, run_and_catch_error_code(
+            ret_codes.append(run_and_catch_error_code(
                 ["python3", vtk_check_script, "-p", _file] + (
                     ["--skip-metadata"] if args["skip_metadata"] else []
                 )
-            ))
+            )[1])
 
             if take_single_sample:
                 print(f"Stopping after first file because '{env_variable_control_name}' was set")
                 break
-    exit(ret_code)
+
+    for c in ret_codes:
+        if c != 0 and c != 42:
+            exit(c)
+    exit(max(ret_codes))
