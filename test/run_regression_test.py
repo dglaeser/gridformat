ddd0ab84c6a86eb673c4fe79987bfbbcfdce812f
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2023-08-10 20:42:06 +0200

    [test][regression] support multiple patterns

--- test/run_regression_test.py
+++ test/run_regression_test.py
@@ -24,6 +24,14 @@ if __name__ == "__main__":
     parser.add_argument("-sm", "--skip-metadata", required=False, action="store_true")
     args = vars(parser.parse_args())
 
+    # fnmatch does not support unix patterns like prefix*+(suffix1|suffix2) as supported by bash...
+    # Therefore, we accept regexes in the form of regex1||regex2, split at the ||, and match against all regexes...
+    # Alternatively, we could make regex an argument that can occur multiple times, but this would require quite some
+    # changes in the cmake scripts...
+    regexes = args["regex"].split("||")
+    def _matches(filename: str) -> bool:
+        return any(fnmatch(filename, _r) for _r in regexes)
+
     mypath = abspath(__file__)
     myfolder = dirname(mypath)
     vtk_check_script = join(myfolder, "test_vtk_file.py")
@@ -39,7 +47,7 @@ if __name__ == "__main__":
     env_variable_control_name = "GRIDFORMAT_REGRESSION_SAMPLES_ONLY"
     take_single_sample = True if getenv(env_variable_control_name, "false").lower() in ["true", "1"] else False
     for _file in filter(lambda _f: isfile(_f), listdir(".")):
-        if fnmatch(_file, f"{args['regex']}"):
+        if _matches(_file):
             print(f"Regression testing '{_file}'")
             ret_codes.append(run_and_catch_error_code(
                 ["python3", vtk_check_script, "-p", _file] + (
