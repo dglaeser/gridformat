d8fd3d7cc769960bb0ef5b5eed545a325cd069ec
Dennis Gläser <dennis.a.glaeser@gmail.com>
2024-11-02 21:54:45 +0100

    [ci] distinguish source/target remotes

--- bin/detect_modified_files.py
+++ bin/detect_modified_files.py
@@ -7,49 +7,65 @@ Prints all files that have been modified between two commits.
 
 import subprocess
 import argparse
-import os
+import shutil
 
 
-def prepare_repo() -> str:
-    _folder_name = "_tmpgfmt"
-    subprocess.run(["git", "clone", "https://github.com/dglaeser/gridformat", _folder_name], check=True, capture_output=True)
-    subprocess.run(["git", "fetch", "origin"], check=True, capture_output=True, cwd=_folder_name)
-    return _folder_name
+def clone_repo_to(
+    directory: str,
+    source_remote: str,
+    target_remote: str
+) -> None:
+    subprocess.run(["git", "clone", source_remote, directory], check=True)
+    subprocess.run(["git", "fetch", "origin"], check=True, cwd=directory)
+    subprocess.run(["git", "remote", "add", "target", target_remote], check=True, cwd=directory)
+    subprocess.run(["git", "fetch", "target"], check=True, cwd=directory)
 
 
-def get_modified_files(source_tree: str, target_tree: str) -> list[str]:
+def is_branch(name: str) -> bool:
+    try:
+        subprocess.run(["git", "show-ref", "--verify", f"refs/heads/{name}"], check=True, capture_output=True)
+        print(f"Found '{name}' to be a branch")
+        return True
+    except:
+        print(f"'{name}' is not a branch")
+        return False
+
+
+def get_modified_files_in(repo_dir: str, source_tree: str, target_tree: str) -> list[str]:
     return subprocess.run(
         ["git", "diff", "--name-only", source_tree, target_tree],
-        check=True, capture_output=True, text=True
+        check=True,
+        capture_output=True,
+        text=True,
+        cwd=repo_dir
     ).stdout.strip(" \n").split("\n")
-
-
-def try_in(folder: str, action):
-    cwd = os.getcwd()
-    try:
-        os.chdir(folder)
-        result = action()
-    except Exception as e:
-        os.chdir(cwd)
-        raise e
-    os.chdir(cwd)
-    return result
 
 
 parser = argparse.ArgumentParser(description="Print the files that have been modified between two commits")
 parser.add_argument("-s", "--source", required=True, help="The source git tree.")
 parser.add_argument("-t", "--target", required=True, help="The target git tree.")
-parser.add_argument(
-    "-i", "--in-place",
-    required=False,
-    action="store_true",
-    help="Use this flag if you want to detect the differences in-place. Per default, the script clones the repo."
-)
+parser.add_argument("-sr", "--source-remote", required=True, help="The url of the remote hosting the source tree")
+parser.add_argument("-tr", "--target-remote", required=True, help="The url of the remote hosting the target tree")
+parser.add_argument("-o", "--out-file", required=True, help="The name of the file in which to write the list of modified files.")
+
 args = vars(parser.parse_args())
 
-source = args["source"]
-target = args["target"]
-modified_files = get_modified_files(source, target) if args["in_place"] else try_in(
-    prepare_repo(), lambda s=source, t=target: get_modified_files(s, t)
+repo_dir = "_tmpgfmt"
+print(f"Cloning gridformat into {repo_dir} (if folder exists it will be deleted)")
+shutil.rmtree(repo_dir, ignore_errors=True)
+clone_repo_to(repo_dir, source_remote=args["source_remote"], target_remote=args["target_remote"])
+
+print("Detecting modified files")
+modified_files = get_modified_files_in(
+    repo_dir,
+    source_tree=f"origin/{args['source']}" if is_branch(args['source']) else args['source'],
+    target_tree=f"origin/{args['target']}" if is_branch(args['target']) else args['target']
 )
-print("\n".join(modified_files))
+
+print("Detected the following modified files:/n")
+print(" -", "\n - ".join(modified_files))
+with open(args["out_file"], "w") as outfile:
+    outfile.write("\n".join(modified_files))
+
+print(f"Cleaning up cloned repo in {repo_dir}")
+shutil.rmtree(repo_dir)
